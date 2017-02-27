3000518a6898e2aefdbf54b2d0b534f2b29053d4
mmz <sucan2015@outlook.com>
2017-02-27 17:30:47 +0800

    websocket的一些测试

--- src/test/java/com/mmz/test/Test_IOC_type.java
+++ src/test/java/com/mmz/test/Test_IOC_type.java
@@ -2,6 +2,7 @@ package com.mmz.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.mmz.entity.Person;
 
@@ -10,7 +11,8 @@ import com.mmz.entity.Person;
 public class Test_IOC_type {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("/src/main/resources/spring.xml");
 		Person person=(Person) ctx.getBean("person");
 		person.getAge();
 	}
